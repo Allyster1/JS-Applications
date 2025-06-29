c97d1181db7b8b2dc01da175a76da472075cb374
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-29 14:19:46 +0300

    Creating folder structure and adding solution for task 1

---
+++ 02. Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
@@ -0,0 +1,30 @@
+async function getInfo() {
+  const stopNameRef = document.getElementById("stopName");
+  const busRef = document.getElementById("buses");
+
+  const stopId = document.getElementById("stopId").value;
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+  try {
+    busRef.replaceChildren();
+
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+      throw Error("Stop Id is not found!");
+    }
+
+    const data = await response.json();
+
+    stopNameRef.textContent = data.name;
+
+    for (const [busNumber, arrivalTime] of Object.entries(data.buses)) {
+      const li = document.createElement("li");
+      li.textContent = `Bus ${busNumber} arrives in ${arrivalTime}`;
+
+      busRef.appendChild(li);
+    }
+  } catch (error) {
+    stopNameRef.textContent = "Error";
+  }
+}
