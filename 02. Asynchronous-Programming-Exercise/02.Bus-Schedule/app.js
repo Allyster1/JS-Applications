1f51efbdfcdf069a32f7053644f268f8d680e21c
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-29 15:09:23 +0300

    Adding solution for task - 02.Bus-Schedule

---
+++ 02. Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
@@ -0,0 +1,44 @@
+function solve() {
+  const departBtn = document.getElementById("depart");
+  const arriveBtn = document.getElementById("arrive");
+
+  const infoTextRef = document.querySelector(".info");
+  let current = "depot";
+  let next = "depot";
+
+  let url = `http://localhost:3030/jsonstore/bus/schedule/${next}`;
+
+  async function depart() {
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+      throw Error("Invalid request");
+    }
+
+    const data = await response.json();
+
+    current = data.name;
+    next = data.next;
+
+    infoTextRef.textContent = `Next stop ${data.name}`;
+
+    departBtn.disabled = true;
+    arriveBtn.disabled = false;
+  }
+
+  async function arrive() {
+    infoTextRef.textContent = `Arriving at ${current}`;
+
+    url = `http://localhost:3030/jsonstore/bus/schedule/${next}`;
+
+    departBtn.disabled = false;
+    arriveBtn.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
+}
+
+let result = solve();
