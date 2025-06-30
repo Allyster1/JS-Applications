3c4ab36d967a6c173c69df8e36c9480431641b74
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-30 21:10:09 +0300

    Updating task to clear used input on submit

--- 02. Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
+++ 02. Asynchronous-Programming-Exercise/01.Bus-Stop/app.js
@@ -2,12 +2,14 @@ async function getInfo() {
   const stopNameRef = document.getElementById("stopName");
   const busRef = document.getElementById("buses");
 
-  const stopId = document.getElementById("stopId").value;
+  const stopIdRef = document.getElementById("stopId");
+
+  const stopId = stopIdRef.value;
   const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
 
   try {
+    stopIdRef.value = "";
     busRef.replaceChildren();
-
     const response = await fetch(url);
 
     if (response.status !== 200) {
