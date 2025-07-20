12526a26f4e1231c6f0310fb9b49cb152e9e4910
Slavi Ovardov <ovardovslav@gmail.com>
2025-07-20 20:49:31 +0300

    adding views for all the pages and applying dynamic navigation

--- exam-prep/03-april-2023/Fruitipedia_Resources/src/app.js
+++ exam-prep/03-april-2023/Fruitipedia_Resources/src/app.js
@@ -1,16 +1,27 @@
 import page from "../node_modules/page/page.mjs";
 
 import { addRender } from "./middlewares/render.js";
+
 import { showHome } from "./views/home.js";
+import { showDashboard } from "./views/dashboard.js";
+import { showCreate } from "./views/create.js";
+import { showSearch } from "./views/search.js";
+import { showDetails } from "./views/details.js";
+import { showEdit } from "./views/edit.js";
+
+import { showRegister } from "./views/register.js";
+import { showLogin } from "./views/login.js";
 
 page(addRender);
 page("/", showHome);
-page("/dashboard", () => console.log("dashboard"));
-page("/search", () => console.log("search"));
-page("/create", () => console.log("create"));
+page("/dashboard", showDashboard);
+page("/search", showSearch);
+page("/create", showCreate);
+page("/details", showDetails);
+page("/edit", showEdit);
 
-page("/login", () => console.log("login"));
-page("/register", () => console.log("register"));
+page("/login", showLogin);
+page("/register", showRegister);
 page("/logout", () => console.log("logout"));
 
 page.start();
---
+++ exam-prep/03-april-2023/Fruitipedia_Resources/src/views/create.js
@@ -0,0 +1,35 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+const createTemplate = () => html` <section id="create">
+  <div class="form">
+    <h2>Add Fruit</h2>
+    <form class="create-form">
+      <input type="text" name="name" id="name" placeholder="Fruit Name" />
+      <input
+        type="text"
+        name="imageUrl"
+        id="Fruit-image"
+        placeholder="Fruit Image"
+      />
+      <textarea
+        id="fruit-description"
+        name="description"
+        placeholder="Description"
+        rows="10"
+        cols="50"
+      ></textarea>
+      <textarea
+        id="fruit-nutrition"
+        name="nutrition"
+        placeholder="Nutrition"
+        rows="10"
+        cols="50"
+      ></textarea>
+      <button type="submit">Add Fruit</button>
+    </form>
+  </div>
+</section>`;
+
+export function showCreate(ctx) {
+  ctx.render(createTemplate());
+}
