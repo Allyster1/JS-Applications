9b9dbd2fb3bceb8ec47a3750319e6cfba3bcc069
Slavi Ovardov <ovardovslav@gmail.com>
2025-07-20 20:25:43 +0300

    adding dynamic routing, render middleware and templating for home page

--- exam-prep/03-april-2023/Fruitipedia_Resources/src/app.js
+++ exam-prep/03-april-2023/Fruitipedia_Resources/src/app.js
@@ -1,1 +1,16 @@
-console.log("works");
+import page from "../node_modules/page/page.mjs";
+
+import { addRender } from "./middlewares/render.js";
+import { showHome } from "./views/home.js";
+
+page(addRender);
+page("/", showHome);
+page("/dashboard", () => console.log("dashboard"));
+page("/search", () => console.log("search"));
+page("/create", () => console.log("create"));
+
+page("/login", () => console.log("login"));
+page("/register", () => console.log("register"));
+page("/logout", () => console.log("logout"));
+
+page.start();
