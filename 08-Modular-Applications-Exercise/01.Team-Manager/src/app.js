2edf8f48dee6f5f4ece57522c77d5f375d585881
Slavi Ovardov <ovardovslav@gmail.com>
2025-07-21 19:50:34 +0300

    adding views

--- 08-Modular-Applications-Exercise/01.Team-Manager/src/app.js
+++ 08-Modular-Applications-Exercise/01.Team-Manager/src/app.js
@@ -2,19 +2,26 @@ import page from "../node_modules/page/page.mjs";
 
 import { addRender } from "./utility/render.js";
 import { updateNav } from "./utility/updateNav.js";
+import { showBrowseTeam } from "./views/browseTeam.js";
+import { showCreate } from "./views/create.js";
+import { showEdit } from "./views/edit.js";
 
 import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
 
 page(addRender);
 page("/", showHome);
-page("/browseTeam", () => console.log("browseTeam"));
+page("/browseTeam", showBrowseTeam);
 page("/myTeams", () => console.log("myTeams"));
 page("/details/:id", () => console.log("details"));
+page("/create", showCreate);
+page("/edit", showEdit);
 
 page("/logout", () => console.log("home"));
-page("/login", () => console.log("login"));
-page("/register", () => console.log("register"));
-
-page.start();
+page("/login", showLogin);
+page("/register", showRegister);
 
 updateNav();
+
+page.start();
